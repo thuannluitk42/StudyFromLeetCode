5c528f80264de48e65dff224b4a5bba80f997efe
thuandevk42@gmail.com <thuandevk42@gmail.com>
2022-02-16 13:42:11 +0700

    88. Merge Sorted Array

--- ChallengeLeetCode/src/Array/MergeSortedArray.java
+++ ChallengeLeetCode/src/Array/MergeSortedArray.java
@@ -25,6 +25,29 @@ public class MergeSortedArray {
 		}
 	}
 
+	// Dung con tro
+	public void merge2(int[] nums1, int m, int[] nums2, int n) {
+		int i = m - 1;
+		int j = n - 1;
+		int k = (m + n) - 1;
+		while (k >= 0) {
+			if (j < 0) {
+				nums1[k] = nums1[i];
+				i--;
+			} else if (i < 0) {
+				nums1[k] = nums2[j];
+				j--;
+			} else if (nums1[i] > nums2[j]) {
+				nums1[k] = nums1[i];
+				i--;
+			} else {
+				nums1[k] = nums2[j];
+				j--;
+			}
+			k--;
+		}
+	}
+
 	public static void main(String[] args) {
 		MergeSortedArray msa = new MergeSortedArray();
 		int[] nums1 = { 1, 2, 3, 0, 0, 0 };
