084fba16c0ed58e5e0d7fa93af70b50ff4d71cfa
thuandevk42@gmail.com <thuandevk42@gmail.com>
2022-02-16 14:34:23 +0700

    1672. Richest Customer Wealth

--- ChallengeLeetCode/src/Array/RichestCustomerWealth.java
+++ ChallengeLeetCode/src/Array/RichestCustomerWealth.java
@@ -5,27 +5,17 @@ public class RichestCustomerWealth {
 	public int maximumWealth(int[][] accounts) {
 		int numberOfCustomer = accounts.length;
 		int numberOfBank = accounts[0].length;
-		int[] sumOfAsset = new int[numberOfCustomer];
+		int max = 0;
 
 		for (int i = 0; i < numberOfCustomer; i++) {
 			int assetOneCustomer = 0;
 			for (int j = 0; j < numberOfBank; j++) {
 				assetOneCustomer += accounts[i][j];
 			}
-			sumOfAsset[i] = assetOneCustomer;
-		}
-		int max = 0;
-		for (int i = 0; i < sumOfAsset.length; i++) {
-			if (sumOfAsset[i] > max) {
-				max = sumOfAsset[i];
+			if (assetOneCustomer > max) {
+				max = assetOneCustomer;
 			}
 		}
 		return max;
 	}
-
-	public static void main(String[] args) {
-		int[][] accounts = { { 1, 2, 3 }, { 3, 2, 1 } };
-
-	}
-
 }
