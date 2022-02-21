c3c1954e7ce2e9280c0aa159565810f508dc1db8
LeVanThuan <thuandevk42@gmail.com>
2022-02-21 22:09:02 +0700

    Binary Search use recursion

--- ChallengeLeetCode/src/BinarySearch/MyBinarySearch.java
+++ ChallengeLeetCode/src/BinarySearch/MyBinarySearch.java
@@ -20,9 +20,31 @@ public class MyBinarySearch {
 		return -1;
 	}
 
-	
+	// binary search dung de quy
+	public static int bSearch(int[] a, int key, int iLeft, int iRight) {
+		if (iLeft > iRight) {
+			return -1;
+		}
+		System.out.println("[" + iLeft + "," + iRight + "]");
+		int iMid = (iLeft + iRight) / 2;
+		if (key == a[iMid]) {
+			return iMid;
+		} else if (key > a[iMid]) {
+			return bSearch(a, key, iMid + 1, iRight);
+		} else { // key < a[iMid]
+			return bSearch(a, key, iLeft, iMid - 1);
+		}
+	}
+
+	public static int bSearch(int[] a, int key) {
+		int n = a.length;
+		return bSearch(a, key, 0, n - 1);
+	}
+
 	public static void main(String[] args) {
 		int[] ar4 = { 1, 2, 3, 4, 5, 7, 8 };
-		System.out.println(binarySearch(ar4, 5));
+		//System.out.println(binarySearch(ar4, 5));
+		System.out.println(bSearch(ar4, 2));
+
 	}
 }
